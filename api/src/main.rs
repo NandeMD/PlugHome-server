8f08cc1a2da8f433501a817d976e7c0e6748cbe4
Umut Can Çelik <celikumutcan2001@gmail.com>
2025-12-25 15:41:15 +0300

    log level should respect RUST_LOG

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,7 +5,8 @@ use axum::{Router, routing::get};
 use chrono::{DateTime, Utc};
 use dotenvy::dotenv;
 use tokio::net;
-use tracing::{Level, info};
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 use occp_ws::routes::{healthcheck_route, upgrade_to_ws};
 use occp_ws::state::START_TIME;
@@ -16,16 +17,18 @@ async fn run() -> Result<()> {
     }
     let _time_now = START_TIME.get_or_init(time_now).await;
 
+    dotenv().ok();
+
     tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
         .with_ansi(true)
         .init();
 
     panic::set_hook(Box::new(|err| {
         tracing::error!("\n\nPanic: {err:#?}\n\n");
     }));
-
-    dotenv().ok();
 
     let addr = env::var("ADDR").context("ADDR must be set")?;
     let port = env::var("PORT").context("PORT must be set")?;
