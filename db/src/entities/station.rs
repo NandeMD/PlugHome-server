c67e0fa0a3a992500ae6c804807d3c9966512807
NandeMD <celikumutcan2001@gmail.com>
2026-04-02 14:02:24 +0300

    db station table schema

---
+++ db/src/entities/station.rs
@@ -0,0 +1,26 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[sea_orm(table_name = "stations")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    #[sea_orm(unique)]
+    pub station_id: String,
+    pub connection_state: StationConnectionState,
+    pub last_seen: DateTimeUtc,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, EnumIter, DeriveActiveEnum)]
+#[sea_orm(rs_type = "String", db_type = "String(Some(8))")]
+pub enum StationConnectionState {
+    #[sea_orm(string_value = "online")]
+    Online,
+    #[sea_orm(string_value = "offline")]
+    Offline,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
